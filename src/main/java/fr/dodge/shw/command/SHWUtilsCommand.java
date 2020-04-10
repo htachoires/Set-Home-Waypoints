f6e5ea860c18a0c92b8b26696c687ab8606584b7
htachoires <hugo.tachoires@etu.u-bordeaux.fr>
2020-04-11 00:06:25 +0200

    fix teleport other dimension

--- src/main/java/fr/dodge/shw/command/SHWUtilsCommand.java
+++ src/main/java/fr/dodge/shw/command/SHWUtilsCommand.java
@@ -3,14 +3,18 @@ package fr.dodge.shw.command;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.world.Teleporter;
+import net.minecraft.world.WorldServer;
 
 public class SHWUtilsCommand {
 
     public static void teleportPlayer(MinecraftServer server, EntityPlayerMP player, String position) {
         String[] data = position.replace(',', '.').split(";");
 
+        WorldServer worldDestination = server.getWorld(Integer.parseInt(data[0]));
+
         if (server.getWorld(Integer.parseInt(data[0])) != server.getWorld(player.getEntityWorld().provider.getDimension())) {
-            server.getPlayerList().changePlayerDimension(player, Integer.parseInt(data[0]));
+            server.getPlayerList().transferPlayerToDimension(player, Integer.parseInt(data[0]), new Teleporter(worldDestination));
         }
         player.connection.setPlayerLocation(Double.parseDouble(data[1]), Double.parseDouble(data[2]),
                 Double.parseDouble(data[3]), Float.parseFloat(data[4]), Float.parseFloat(data[5]));
